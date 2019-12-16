e00c427a0ed400a366079cc577c3e3e2b7af94b3
Luca Cattide <l.cattide@email.it>
2019-12-16 14:03:36 +0100

    Upgrades: - Babel implementation; - PWA implementation; - Jest implementation;
    Updates:
    - Readme;
    - Code of conduct;
    - Contacts;
    - Rich Snippets;

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -15,12 +15,21 @@
 <meta name="og:locale" property="og:locale" content="it_IT" />
 <meta name="og:locale:alternate" property="og:locale:alternate" content="" />
 <meta name="og:site_name" property="og:site_name" content="" />
+<meta name="apple-mobile-web-app-capable" content="yes">
+<meta name="apple-mobile-web-app-status-bar-style" content="">
+<meta name="apple-mobile-web-app-title" content="">
+<meta name="theme-color" content="#" />
 <!--Fine Meta Tags-->
 <!--Inizio Preload-->
 <link rel="preload" href="css/app.css" as="style">
 <link rel="preload" href="js/app.js" as="script">
+<link rel="preload" href="manifest.json" as="script">
+<link rel="preload" href="js/refresh.js" as="script">
+<link rel="preload" href="js/install.js" as="script">
 <!--Fine Preload-->
 <!--Inizio Importazione Stili-->
 <link rel="stylesheet dns-prefetch" href="css/app.css">
 <!--Fine Importazione Stili-->
 <link rel="icon dns-prefetch" type="image/png" href="favicon.png">
+<!--TODO: Must be 152x152 px PNG-->
+<link rel="apple-touch-icon dns-prefetch" href="img/">
